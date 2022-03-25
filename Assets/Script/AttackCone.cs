8c1000552eab74255d2819f774ecf81f7a4ed5f7
Khainq0 <77739027+Khainq0@users.noreply.github.com>
2022-03-25 15:56:37 +0700

    Initial commit

---
+++ Assets/Script/AttackCone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCone : MonoBehaviour
+{
+    public DragonAI dragon;
+    public bool isLeft = false;
+
+
+    private void Awake()
+    {
+        dragon = gameObject.GetComponentInParent<DragonAI>();
+
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (isLeft)
+                dragon.Attack(false);
+
+            else
+                dragon.Attack(true);
+
+
+        }
+    }
+}
---
+++ Assets/Script/BOX1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOX1 : MonoBehaviour
+{
+    public int Health = 100;
+    public bool death = false;
+    public Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        if (Health <= 0)
+        {
+            death = true;
+            anim.SetBool("Death", death);
+            Destroy(gameObject, 1);
+        }
+    }
+
+    void Damage(int damage)
+    {
+        Health -= damage;
+    }
+}
